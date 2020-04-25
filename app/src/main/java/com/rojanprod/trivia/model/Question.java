559892bba0742f34ad5e2fa25f59caeb325b9dcd
Rojan <maxpanye60@gmail.com>
2020-04-25 23:46:45 +0545

    App Update

--- app/src/main/java/com/rojanprod/trivia/model/Question.java
+++ app/src/main/java/com/rojanprod/trivia/model/Question.java
@@ -1,5 +1,12 @@
 package com.rojanprod.trivia.model;
 
+/**
+ * This class is used to represent the single question of the trivia quiz questions
+ * When an array of questions are extracted from Json request then the individual question are the
+ * instance of this class where the individual question contains a questionName and an answer as 'true' or 'false'
+ * This class is used to set the individual question and to extract the individual question and answer
+ * for a single element in the question list array
+ */
 public class Question {
     private String questionName;
     private boolean answer;
